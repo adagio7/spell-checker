fc7dfe5677b2c4f368c1eda6d900f79d82404819
adagio7 <haojiepe@gmail.com>
2024-05-16 16:22:55 +0100

    feat: setup cli flags for dict_path and verbose

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
-use clap::Parser;
+use clap::{ Parser, arg, command };
 use std::collections::HashSet as Hashset;
 
 mod utils;
 mod algorithms;
 
+use utils::load_dictionary;
 use algorithms::base::SpellChecker;
 use algorithms::levenshtein::Levenshtein;
 
-#[derive(Parser, Debug)]
-struct Args {
-    #[arg(short, long)]
-    dictionary_path: String, 
-
-    #[arg(short, long)]
-    verbose: bool,
-
-    // #[arg(subcommand)]
-    // cmd: Command,
-}
-
 fn main() {
-    // let args = Args::parse();
+    // TODO: Let user specify the dictionary module_path!
+    // TODO: Let user specify the algorithm to use!
+    // TODO: Let user toggle default matches
 
-    let dictionary: Result<Hashset<String>, std::io::Error> = utils::load_dictionary("./dictionaries/google-10k-eng.txt");
+    let matches =
+        command!("spell_check")
+            .arg(
+                arg!(-d --dictionary_path <path> "Path to the dictionary file")
+                    .default_value("./dictionaries/google-10k-eng.txt")
+            )
+            .arg(
+                arg!(-v --verbose "Prints debug information verbosely")
+            )
+            .subcommand(
+                command!("levenshtein").about("Uses the Levenshtein algorithm to spellcheck")
+            )
+            .get_matches();
+
+
+    let dictionary: Result<Hashset<String>, std::io::Error> = load_dictionary(matches.get_one::<String>("dictionary_path").unwrap());
 
     let spell_checker = Levenshtein{
         default_matches: 5,
