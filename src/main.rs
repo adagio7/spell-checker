930d7bd10344ed82aa0bbcc9a3e172ae3fd67d13
adagio7 <haojiepe@gmail.com>
2024-05-16 01:11:50 +0100

    feat: added clap for CLI parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    dictionary_path: String, 
+
+    #[arg(short, long)]
+    verbose: bool,
+
+    // #[arg(subcommand)]
+    // cmd: Command,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Dictionary path: {}", args.dictionary_path);
+    println!("Verbose: {}", args.verbose);
 }
